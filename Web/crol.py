235ee04c4e8c981a4ce461300b27c53c54c71b0a
Wonho ha <haho013039@gmail.com>
2019-07-03 12:15:58 +0900

    remove chrome options

--- Web/crol.py
+++ Web/crol.py
@@ -3,17 +3,13 @@ from time import sleep
 import asyncio
 
 driver_Path = "../../chromedriver"
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
 
 
 def get_right_nobel(article):
     if article.is_exist:
         article.now = article.no
         return
-    driver = webdriver.Chrome(driver_Path, chrome_options=options)
+    driver = webdriver.Chrome(driver_Path)
     driver.implicitly_wait(2)
 
     link = f"https://m.blog.naver.com/{article.blog}/{article.first}"
